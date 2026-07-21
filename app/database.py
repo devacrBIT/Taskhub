a184bc111d96ab290a73823cdea4b6c8a924fb40
devacrBIT <ggournn@proton.me>
2026-07-21 23:24:37 +0300

    Fix database folder creation

--- app/database.py
+++ app/database.py
@@ -1,15 +1,33 @@
 import sqlite3
+from pathlib import Path
 
-DB_NAME = "data/tasks.db"
+
+DATA_DIR = Path("data")
+
+DATA_DIR.mkdir(
+    exist_ok=True
+)
+
+
+DB_NAME = DATA_DIR / "tasks.db"
+
 
 def get_connection():
+
     conn = sqlite3.connect(DB_NAME)
-    conn.row_factory = sqlite3.Row #Return rows like dictionaries
+
+    conn.row_factory = sqlite3.Row  # Return rows like dictionaries
+
     return conn
 
+
+
 def init_db():
+
     conn = get_connection()
+
     cursor = conn.cursor()
+
 
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS tasks (
@@ -23,5 +41,7 @@ def init_db():
         )
     """)
 
+
     conn.commit()
+
     conn.close()
